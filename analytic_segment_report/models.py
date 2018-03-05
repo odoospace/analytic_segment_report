235abb74279c990dca88dc9e1ea37d8ee76076e3
Javier Colmeiro <javir.colmeiro@impulzia.com>
2018-03-05 17:17:39 +0100

    class changes

--- analytic_segment_report/models.py
+++ analytic_segment_report/models.py
@@ -3,16 +3,16 @@ from openerp.tools.translate import _
 
 class AccountReportGeneralLedgerWizard(models.TransientModel):
 
-    _inherit = "account.common.account.report"
+    _inherit = "general.ledger.webkit"
 
-    analytic_segment_ids = fields.One2many('account.common.account.report.segments', 'report_id')
+    analytic_segment_ids = fields.One2many('general.ledger.webkit.segments', 'report_id')
 
 
 class AccountReportGeneralLedgerWizardSegments(models.TransientModel):
 
-    _name = "account.common.account.report.segments"
+    _name = "general.ledger.webkit.segments"
 
-    report_id = fields.Many2one('account.common.account.report')
+    report_id = fields.Many2one('general.ledger.webkit')
     segment_id = fields.Many2one('analytic_segment.segment') #, required=True)
     segment = fields.Char(related='segment_id.segment', readonly=True)
     with_children = fields.Boolean(default=False)
