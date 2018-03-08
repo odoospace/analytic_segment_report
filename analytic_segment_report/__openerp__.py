bc09b85a7aca7c1e50b64da776a85220016cb5e8
Javier Colmeiro <javir.colmeiro@impulzia.com>
2018-03-08 09:00:48 +0100

    + add trial balance

--- analytic_segment_report/__openerp__.py
+++ analytic_segment_report/__openerp__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '8.0.0.1',
+    'version': '8.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account', 'analytic_segment'],
--- analytic_segment_report/models.py
+++ analytic_segment_report/models.py
@@ -1,6 +1,7 @@
 from openerp import tools, models, fields, api
 from openerp.tools.translate import _
 
+#General Ledger - libro mayor
 class AccountReportGeneralLedgerWizard(models.TransientModel):
 
     _inherit = "general.ledger.webkit"
@@ -16,3 +17,21 @@ class AccountReportGeneralLedgerWizardSegments(models.TransientModel):
     segment_id = fields.Many2one('analytic_segment.segment') #, required=True)
     segment = fields.Char(related='segment_id.segment', readonly=True)
     with_children = fields.Boolean(default=False)
+
+#Trial balance - sumas y saldos
+class AccountTrialBalanceWizard(models.TransientModel):
+
+    _inherit = "trial.balance.webkit"
+
+    analytic_segment_ids = fields.One2many('general.ledger.webkit.segments', 'report_id')
+
+
+class AccountReportTrialBalanceWizardSegments(models.TransientModel):
+
+    _name = "trial.balance.webkit.segments"
+
+    report_id = fields.Many2one('general.ledger.webkit')
+    segment_id = fields.Many2one('analytic_segment.segment') #, required=True)
+    segment = fields.Char(related='segment_id.segment', readonly=True)
+    with_children = fields.Boolean(default=False)
+
